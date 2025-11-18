77ca3a71e69364983d7c90f01fdfffaf6c9bd3b5
Tivsue Tavershima <tivsuetavershima@gmail.com>
2025-11-18 20:46:44 +0100

    new files

--- market/urls.py
+++ market/urls.py
@@ -7,6 +7,9 @@ from market import views
 
 
 urlpatterns = [
-    path('get_product/',views.get_product)
+    path('get_product/',views.get_product),
+    path('create_product/',views.create_product),
+    path('update_product/<int:id>/',views.update_product),
+    path('delete_product/<int:id>/',views.delete_product),
     
 ]
